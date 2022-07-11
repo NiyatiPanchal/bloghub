751e03ef4414fb72761e253a79e6e3bb8a475cf3
Niyati Panchal <niyatipanchal85@gmail.com>
2022-07-11 21:32:01 +0530

    Added Functionality

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
-
 require("dotenv").config();
-const mongoURI = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@bloghub.rqplb4n.mongodb.net/?retryWrites=true&w=majority`;
+const mongoURI = `${process.env.MONGO_URI}`;
 // Returns promise
 const connectDB = async () => {
   mongoose.connect(mongoURI, () => console.log("Connected to MongoDB"));
--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,29 +1,11 @@
-// import multer from "multer";
-// import { GridFsStorage } from "multer-gridfs-storage";
-
-// const storage = new GridFsStorage({
-//   url: " mongodb+srv://user:999996@bloghub.rqplb4n.mongodb.net/?retryWrites=true&w=majority",
-//   options: { useNewUrlParser: true },
-//   file: (request, file) => {
-//     const match = ["image/png", "image/jpg"];
-
-//     if (match.indexOf(file.memeType) === -1)
-//       return `${Date.now()}-blog-${file.originalname}`;
-
-//     return {
-//       bucketName: "photos",
-//       filename: `${Date.now()}-blog-${file.originalname}`,
-//     };
-//   },
-// });
-
-// export default multer({ storage });
-
 const multer = require("multer");
 const { GridFsStorage } = require("multer-gridfs-storage");
+require("dotenv").config();
+
+const mongoURI = `${process.env.MONGO_URI}`;
 
 const storage = new GridFsStorage({
-  url: "mongodb+srv://user:999996@bloghub.rqplb4n.mongodb.net/?retryWrites=true&w=majority",
+  url: "mongoURI",
   options: { useNewUrlParser: true },
   file: (req, file) => {
     const match = ["image/png", "image/jpg"];
