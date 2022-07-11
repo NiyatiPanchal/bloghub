78804a61f31261a246cdccc52a170551a965c0a1
Niyati Panchal <niyatipanchal85@gmail.com>
2022-07-11 23:46:20 +0530

    final commit

--- backend/index.js
+++ backend/index.js
@@ -4,10 +4,11 @@ var cors = require("cors");
 const app = express();
 require("dotenv").config();
 const bodyParser = require("body-parser");
+const path = require("path");
 
 app.use(cors());
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 connectDB();
 
@@ -19,6 +20,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/auth", require("./routes/auth.js"));
 app.use("/api/posts", require("./routes/posts.js"));
 app.use("/api/comments", require("./routes/comments.js"));
+
+// -------Deployment--------
+__dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 app.listen(port, () => {
   console.log(`BlogHub backend listening on port ${port}`);
